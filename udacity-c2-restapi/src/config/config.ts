bff57c737d44a2809f771c93279681d715c80237
heniam <heniam2012@gmail.com>
2020-05-08 02:32:23 -0400

    Update config.ts

--- udacity-c2-restapi/src/config/config.ts
+++ udacity-c2-restapi/src/config/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   "dev": {
-    "username": "udagramhenokdev",
-    "password": "udagramhenokdev",
-    "database": "udagramhenokdev",
-    "host": "udagramhenokdev.cyf1w3cd5dum.us-east-1.rds.amazonaws.com",
+    "username": "udagramheniamdev",
+    "password": "udagramheniamdev",
+    "database": "udagramheniamdev",
+    "host": "udagramheniamdev.cyf1w3cd5dum.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-1",
     "aws_profile": "default",
