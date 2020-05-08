02da864d1362cb06879db596d3abd98773311949
heniam <heniam2012@gmail.com>
2020-05-07 23:04:56 -0400

    config file

---
+++ udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramhenokdev",
+    "password": "udagramhenokdev",
+    "database": "udagramhenokdev",
+    "host": "udagramhenokdev.cyf1w3cd5dum.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagramhenokdev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
