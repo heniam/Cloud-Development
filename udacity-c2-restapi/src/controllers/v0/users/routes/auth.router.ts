9bd47d392ffa8232bcc75ac37a4a6b3a164518ee
heniam <heniam2012@gmail.com>
2020-05-09 00:12:00 -0400

    Logs and having issue with ElasticBeans

--- udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -12,14 +12,19 @@ const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
     //@TODO Use Bcrypt to Generated Salted Hashed Passwords
+    const salts = 10;
+    let salt = await bcrypt.genSalt(salts);
+    return await bcrypt.hash(plainTextPassword, salt);
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
     //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
+    return bcrypt.compare(plainTextPassword,hash);
 }
 
 function generateJWT(user: User): string {
     //@TODO Use jwt to create a new JWT Payload containing
+    return jwt.sign(user.short(), "helloworld")
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
